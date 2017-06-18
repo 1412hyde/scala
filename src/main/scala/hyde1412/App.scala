7695dfe9cbc9f48cb090d2b59654dab4753bd2ef
1412hyde <13953246933@126.com>
2017-06-18 20:10:35 +0800

    Initial commit

---
+++ src/main/scala/hyde1412/App.scala
@@ -0,0 +1,9 @@
+package hyde1412
+
+/**
+ * Hello world!
+ *
+ */
+object App extends Application {
+  println( "Hello World!" )
+}
---
+++ src/test/scala/hyde1412/AppTest.scala
@@ -0,0 +1,17 @@
+package hyde1412
+
+import org.junit._
+import Assert._
+
+@Test
+class AppTest {
+
+    @Test
+    def testOK() = assertTrue(true)
+
+//    @Test
+//    def testKO() = assertTrue(false)
+
+}
+
+
